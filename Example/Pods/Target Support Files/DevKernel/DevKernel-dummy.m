3da3d07c46fbd32dcca9b2dcdb95dfafe8bc577f
常润泽 <changrz1@centaline.com.cn>
2017-09-05 17:54:58 +0800

    update new tools
    Signed-off-by: 常润泽 <changrz1@centaline.com.cn>

---
+++ Example/Pods/Target Support Files/DevKernel/DevKernel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DevKernel : NSObject
+@end
+@implementation PodsDummy_DevKernel
+@end
---
+++ Example/Pods/Target Support Files/Pods-DevKernel_Tests/Pods-DevKernel_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevKernel_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DevKernel_Tests
+@end
